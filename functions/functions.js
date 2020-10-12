7c552fb89a0547d6cac6563227c5eee7ad7db5de
Dawid Wroński <davvid96@gmail.com>
2020-10-12 20:05:48 +0200

    Add functions dir with files

---
+++ functions/functions.js
@@ -0,0 +1,45 @@
+// const showMessage = function (){
+//     console.log('Show me!');    
+// };
+// showMessage();
+
+// const showDegree = function (degree){
+//     console.log(degree);    
+// };
+// showDegree('Capitan');
+
+// const soldier = ['Luis', 'Ben', 'Jack'];
+// const age = [24,20,31];
+
+// const showSoldierData = function (name){
+//     console.log(`Name: ${name}`);
+//     return name;
+// }
+//  soldier.push(showSoldierData('Alfred'));
+
+// function declaration
+
+// function addNumbers(x,y){
+//     return x+y;
+// }
+// const result = addNumbers(4,4);
+// console.log(result);
+
+// function constructor
+//better dont use
+
+
+// ARROW FUNCTION EC6
+
+// const logText = (text) => {
+    // console.log(text);    
+// }
+// logText('First way works fine!')
+
+// or even shorter
+// const logText = text => console.log(text);
+// logText('Second way is shorter!');
+
+// return how much parameters is declared on function declaration
+// console.log(logText.length);
+
