b9f6b91be778cb782fac4b5d15122d89b3412d3b
Dawid Wroński <davvid96@gmail.com>
2020-10-14 14:27:33 +0200

    practice selecting DOM items

---
+++ DOM/DOM.js
@@ -0,0 +1,28 @@
+
+//selecting items from DOM with new and older methods;
+
+const header2 = document.querySelector('h2');
+const header2Alter = document.querySelector('h1~ul+h2')
+const header1 = document.getElementById('header');
+const li = document.getElementsByTagName('li');
+const loremP = document.getElementsByClassName('lorem');
+
+// textContent and innerHtml practice
+
+// console.log(header1);
+// console.log(header1.textContent);
+// const liArray = [...li];
+// console.log(li[2].innerHTML);
+// console.log(loremP);
+// console.log(header2.textContent);
+// console.log(header2Alter.textContent);
+
+
+
+
+
+
+
+
+
+
