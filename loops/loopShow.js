8902e7ec43afbae7c1c6b211d6c4754ba2a03c61
Dawid Wroński <davvid96@gmail.com>
2020-10-20 11:03:46 +0200

    test foreach parameters

--- loops/loopShow.js
+++ loops/loopShow.js
@@ -5,20 +5,30 @@ const li = document.querySelectorAll('li');
 let fontSize = 16;
 let a = 15;
 
-btn.addEventListener('click',()=>{
-    for(i = 0; i < li.length; i++){
-        li[i].style.display = 'block';
-        li[i].style.fontSize = `${fontSize}px`;
-    }
-    fontSize += 2;   
-})
+// btn.addEventListener('click',()=>{
+//     for(i = 0; i < li.length; i++){
+//         li[i].style.display = 'block';
+//         li[i].style.fontSize = `${fontSize}px`;
+//     }
+//     fontSize += 2;   
+// })
 
 // foreach
 // btn.addEventListener('click',()=>{
-//     li.forEach((item)=>{
+//     li.forEach((item, index, monkey, apple)=>{
+//         console.log(item);
+//         console.log(index);
+//         console.log(monkey);
+//         console.log(apple);       
 //         item.style.display = "block";
 //         item.style.fontSize = `${fontSize}px`;
 //     })    
 //     fontSize += 2;    
 // })
 
+// const arr = ['a','b','c'];
+// arr.forEach((z, a, c) =>{
+//     console.log(z);
+//     console.log(a);
+//     console.log(c);    
+// } )
