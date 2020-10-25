5f6f47047fa7c04e447cc383a1e0d4c8ac21fb94
Dawid Wroński <davvid96@gmail.com>
2020-10-25 12:00:00 +0100

    add delete name function

--- course exercises/randomName.js
+++ course exercises/randomName.js
@@ -1,11 +1,20 @@
-const btn = document.querySelector('button');
-const div = document.querySelector('div');
-const names = ['Weronika', 'Karolina','Agata','Asia', 'Patrycja','Kasia','Ola','Emila','Justyna','Anastazja'];
+const btn = document.querySelector('.draw');
+const deleteBtn = document.querySelector('.delete');
+const div = document.querySelector('.result');
+const names = ['Weronika', 'Karolina','Agata','Asia','Patrycja','Kasia','Ola','Emila','Justyna','Anastazja'];
 const prefixes = ['Najlepsze imię to','Twoja córka ma na imię','Dziewczynka powinna się nazywać','To na 100%'];
 
 const drawName = () =>{
     // console.log(Math.random());
-    div.textContent = `${prefixes[Math.floor([Math.random() * prefixes.length])]} 
-    ${names[Math.floor(Math.random() * names.length)]}`;
+    if(names.length > 0){
+        nameIndex = Math.floor(Math.random() * names.length);
+        div.textContent = `${prefixes[Math.floor([Math.random() * prefixes.length])]} ${names[nameIndex]}`; 
+    }
+    else 
+    div.textContent = 'Nie można Cię zadowolić :)';
+       
 }
+deleteBtn.addEventListener('click', deleteName = ()=>{  
+    names.splice(nameIndex,1);                            
+})
 btn.addEventListener('click', drawName);
