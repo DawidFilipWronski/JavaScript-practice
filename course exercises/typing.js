7146af9d94d479d12db84c88a6d27d31dcd1a3e8
Dawid Wroński <davvid96@gmail.com>
2020-11-05 13:10:02 +0100

    add typing text exercise

---
+++ course exercises/typing.js
@@ -0,0 +1,47 @@
+// http://websamuraj.pl/examples/js/projekt8/
+// Uzyskaj efekt jak pod tym linkiem 
+// Użyj setTimeout zamiast setInterval,
+// Opóźniej start pisanie każdego słowa
+// Uwaga: Zadanie bardzo trudne
+
+const spnText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor');
+const txt = ['tekst1', 'tekst2', 'tekst3'];
+
+let activeText = 0;
+let activeLetter = 0;
+
+// Implementacja
+const clear = () =>{
+    spnText.textContent = '';
+}
+const addLetter = () => {
+ // Użyj w środku setTimeout
+ if(activeLetter >= 0){
+     spnText.textContent += txt[activeText][activeLetter];
+    }
+ 
+ activeLetter++;
+ if(activeLetter === txt[activeText].length){
+    activeText++;
+    if(activeText === txt.length) return;
+    return setTimeout(()=>{
+    activeLetter = -4;
+    spnText.textContent = '';
+    addLetter();
+    }, 2000)
+    
+ }
+ setTimeout(addLetter,500) 
+}
+
+
+addLetter(); //pierwsze wywołanie
+
+
+// Animacja kursora (zostaw bez zmian)
+const cursorAnimation = () => {
+ spnCursor.classList.toggle('active');
+}
+setInterval(cursorAnimation, 400);
+
